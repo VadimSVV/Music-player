23fc33e5eea54135cb0c2139184fee4a87059a37
VadimSVV <korsarishe@gmail.com>
2023-04-12 07:34:40 +0300

    Functions

---
+++ js/music-list.js
@@ -0,0 +1,63 @@
+
+let allMusic = [
+  {
+    name: "Track 1",
+    artist: "",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Track 2",
+    artist: "",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Track 3",
+    artist: "",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Track 4",
+    artist: "",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Track 5",
+    artist: "",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Track 6",
+    artist: "",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Track 7",
+    artist: "",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Track 8",
+    artist: "",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Track 9",
+    artist: "",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Track 10",
+    artist: "",
+    img: "music-10",
+    src: "music-10"
+  },
+];
